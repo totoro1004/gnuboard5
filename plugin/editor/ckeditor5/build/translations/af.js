6e6e12c399feac2cb6ebefd0bc3312fd22be01cd
projectSylas <didldldl@naver.com>
2021-12-29 10:56:59 +0900

    ckeditor test

---
+++ plugin/editor/ckeditor5/build/translations/af.js
@@ -0,0 +1,1 @@
+(function(e){const r=e["af"]=e["af"]||{};r.dictionary=Object.assign(r.dictionary||{},{Bold:"Vetgedruk",Cancel:"Kanselleer",Italic:"Skuinsgedruk","Remove color":"",Save:"Berg",Strikethrough:"Deurgetrek",Underline:"Onderstreep"});r.getPluralForm=function(e){return e!=1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
---
+++ plugin/editor/ckeditor5/src/ckeditor.js
@@ -0,0 +1,41 @@
+/**
+ * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle.js';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Bold,
+	Essentials,
+	FontBackgroundColor,
+	FontColor,
+	Image,
+	ImageUpload,
+	Italic,
+	Link,
+	List,
+	ListStyle,
+	MediaEmbed,
+	Paragraph,
+	Strikethrough,
+	Underline
+];
+
+export default Editor;
